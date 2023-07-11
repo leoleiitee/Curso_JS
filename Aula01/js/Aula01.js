dd253726f65eea5a9e40f7b266ea0fae57f8134c
leoleiitee <leo.rleite@outlook.com>
2023-07-11 01:28:16 -0300

    add - finalizado aula01

---
+++ Aula01/js/Aula01.js
@@ -0,0 +1,6 @@
+/*
+    Arquivo esterno de js
+*/
+document.write("Ola mundo!");    //Escreve algo no documento
+console.log("Ola mundo");        //Escreve algo no console
+alert("Ola mundo!");             //Escreve algo em modo de alerta
